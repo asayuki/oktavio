d3881a48fdcdc51da2d44d09e4775f2fd139df7b
erth <erik.thunberg@adeprimo.se>
2016-09-26 16:40:20 +0200

    Beginning of modes

---
+++ core/api/modes/model/mode.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const Mongoose = require('mongoose');
+const Schema = Mongoose.Schema;
+
+const modeModel = new Schema({
+  name: {
+    type: String,
+    required: true,
+    index: {
+      unique: true
+    }
+  },
+  devices: [{
+    id: {
+      type: Schema.Types.ObjectId,
+      required: true
+    },
+    state: {
+      type: Boolean,
+      required: true
+    }
+  }],
+  active: {
+    type: Boolean,
+    required: true
+  }
+});
+
+modeModel.index({
+  name: 'text'
+});
+
+module.exports = Mongoose.model('Mode', modeModel);
